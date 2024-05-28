b0c4cc25ef6264ecfac2b24c5a1b538913bfbf0c
Spacefox95 <nathan.raynal@gmail.com>
2024-05-28 16:00:56 +0200

    Update status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  const selectedAmen = {};
+
+ $('input[type="checkbox"]').change(function() {
+  if (this.checked) {
+    selectedAmen[$(this).data('id')]=$(this).data('name');
+  } else {
+    delete selectedAmen[$(this).data('id')];
+  }
+
+  const amenList = Object.values(selectedAmen).join(', ');
+  $('div.amenities h4').text(amenList);
+ });
+});
+
+function checkApiStatus() {
+	$.get('http://0.0.0.0:5001/api/v1/status/')
+	.done(function(data) {
+		if (data.status === 'OK') {
+			$('#api_status').addClass('available');
+		} else {
+			$('#api_status').removeClass('available');
+		}
+	})
+	.fail(function() {
+		$('#api_status').removeClass('available');
+	});
+}
+
+checkApiStatus();
